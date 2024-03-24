0639ca9eeb61539ee8e975e0ece99aefb9aec857
tonydefaria <me@tonydefaria.com>
2024-03-24 15:38:02 +0100

    clear bug

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,13 @@ export default function Index() {
       <div className="page writer">
         <h1>A New Approach to Content Management</h1>
         <p>
-          In today's fast-paced world, managing digital content shouldn’t be a hurdle—it should be a pleasure. That’s the core belief driving <strong>hankyō</strong>, your new partner in the world of Headless CMS.
+          In today&apos;s fast-paced world, managing digital content shouldn&apos;t be a hurdle—it should be a pleasure. That&apos;s the core belief driving <strong>hankyō</strong>, your new partner in the world of Headless CMS.
         </p>
         <p>
           Unlike traditional CMS platforms that overwhelm with complexity, <strong>hankyō</strong> embraces minimalism. By streamlining the content management process, <strong>hankyō</strong> enables you to concentrate on what truly matters: your vision, your content, your peace.
         </p>
         <p>
-          Designed for micro- to small-scale projects, <strong>hankyō</strong> is for everyone who craves simplicity without sacrificing their project goals. Whether you're a web designer, a startup founder, or a developer, <strong>hankyō</strong> empowers you to achieve more with less.
+          Designed for micro- to small-scale projects, <strong>hankyō</strong> is for everyone who craves simplicity without sacrificing their project goals. Whether you&apos;re a web designer, a startup founder, or a developer, <strong>hankyō</strong> empowers you to achieve more with less.
         </p>
         <p>
           The upcoming launch of <strong>hankyō</strong> is in early summer 2024. We invite you to join our invitation list today for early access.
